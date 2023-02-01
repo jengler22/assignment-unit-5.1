5b753aa78e8343078a7966e32165a8f00f61ccb7
Jakob Engler <jengler2291@gmail.com>
2023-01-31 20:30:55 -0600

    Start

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Jake',
+  lastName: 'Engler',
+  hasSiblings: 'True',
+  shoeCount: 6,
+  faveThreeFoods: ['taco', 'pizza', 'steak'],
 };
 console.log('A little about me:', me);
 
@@ -19,6 +24,7 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+
 
 
 
